0f737e0a9381b181253e3a4b90f3fcc589567d0d
Vedat Akcan <akcanvedat47@gmail.com>
2025-04-19 16:06:35 +0300

    Refactor: StartFragment düzenlendi, ViewBinding doğru şekilde uygulandı.

--- app/src/main/java/com/vedatakcan/inomaker/StartFragment.kt
+++ app/src/main/java/com/vedatakcan/inomaker/StartFragment.kt
@@ -2,52 +2,52 @@ package com.vedatakcan.inomaker
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.vedatakcan.inomaker.databinding.FragmentStartBinding
 
 
 class StartFragment : Fragment() {
-    private lateinit var binding: FragmentStartBinding
+
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
     private lateinit var navController: NavController
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        navController = Navigation.findNavController(view)
         super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        binding.btnStart.setOnClickListener {
+            navController.navigate(R.id.action_startFragment_to_optionsFragment)
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        // Get the ActionBar
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
     }
 
     override fun onStop() {
         super.onStop()
-        // Hide the ActionBar
         (requireActivity() as AppCompatActivity).supportActionBar?.show()
     }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentStartBinding.inflate(inflater, container, false)
 
-        binding.btnStart.setOnClickListener {
-            navController.navigate(R.id.action_startFragment_to_optionsFragment)
-        }
-
-
-        return binding.root
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
